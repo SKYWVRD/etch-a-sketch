9682a4d2b0c44a6877604a47e0d4ae8a557f4528
Sean Boonzaier <boonzaiersa@gmail.com>
2022-07-09 21:21:35 +0200

    Add js logic to insert rows

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const mainContainer = document.querySelector(".main-container");
+
+
+mainContainer.style.cssText = "border-style: solid; display:flex; flex-direction: column;"
+
+for (let i = 0; i < 16; i++){
+    const row = document.createElement('div');
+    row.textContent = `Row ${i}`
+    row.setAttribute('class', 'row')
+    for (let i = 0; i < 16; i++){
+        console.log(i);
+    }
+
+    mainContainer.appendChild(row);
+}
