6c6f6601e35e5e572c98f3507c10c85770389e45
Sean Boonzaier <boonzaiersa@gmail.com>
2022-07-12 20:02:18 +0200

    Change pixel size

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const mainContainer = document.querySelector(".main-container");
-const resolution = 32; //Change to max it an resolution x resolution sized board
+const resolution = 64; //Change to max it an resolution x resolution sized board
 
 
-mainContainer.style.cssText = "border-style: solid; display:inline-flex; flex-direction: column; align-items: centre";
+mainContainer.style.cssText = "border-style: solid; display:inline-flex; flex-direction: column;";
 
 for (let i = 0; i < resolution; i++){
     const row = document.createElement('div');
@@ -12,7 +12,7 @@ for (let i = 0; i < resolution; i++){
     for ( let j = 0; j < resolution; j++ ){
         const box = document.createElement('div');
         box.setAttribute('class', 'box');
-        box.style.cssText = "border-style: solid; border-width: 0.5px; height: 10px; width: 10px;";
+        box.style.cssText = "border-style: solid; border-width: 0.5px; height: 7.5px; width: 7.5px;";
         box.addEventListener('mouseover', () => {
             box.style.backgroundColor = "black";
         });
