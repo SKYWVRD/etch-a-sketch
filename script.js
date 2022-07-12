7b35aa73b8df8e066d2c90a8c4a8c696903a388d
Sean Boonzaier <boonzaiersa@gmail.com>
2022-07-12 19:52:06 +0200

    Add drawing functionality and resolution variable

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const mainContainer = document.querySelector(".main-container");
+const resolution = 32; //Change to max it an resolution x resolution sized board
 
 
-mainContainer.style.cssText = "border-style: solid; display:inline-flex; flex-direction: column"
+mainContainer.style.cssText = "border-style: solid; display:inline-flex; flex-direction: column";
 
-for (let i = 0; i < 16; i++){
+for (let i = 0; i < resolution; i++){
     const row = document.createElement('div');
-    row.style.cssText = "display: flex;"
-    row.setAttribute('class', 'row')
+    row.style.cssText = "display: flex;";
+    row.setAttribute('class', 'row');
 
-    for ( let j = 0; j < 16; j++ ){
+    for ( let j = 0; j < resolution; j++ ){
         const box = document.createElement('div');
-        box.style.cssText = "border-style: solid; height: 10px; width: 10px;"
+        box.setAttribute('class', 'box');
+        box.style.cssText = "border-style: solid; border-width: 0.5px; height: 10px; width: 10px;";
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = "black";
+        });
         row.appendChild(box);
     }
 
     mainContainer.appendChild(row);
 }
+
+const pixels = document.querySelectorAll(".box");
+
